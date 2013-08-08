1d5ac404a7948ca672a2ccd27a67ee22e7efa3c6
bostonlink <bostonlink@pentest-labs.org>
2013-08-08 19:27:52 -0400

    updated self hosted

--- cloudPWN.py
+++ cloudPWN.py
@@ -8,6 +8,7 @@ import core.lib.ec2funky as ec2funky
 import core.lib.fabfunky as fabfunky
 import core.modules.setweb as setweb
 import core.menus as menus
+import core.lib.selfy
 from fabric.colors import red, yellow
 
 __author__ = 'David Bressler (@bostonlink), GuidePoint Security LLC'
@@ -29,7 +30,7 @@ instance_type = config["instance_type"]
 
 # Select what service to launch and get the instance id
 try:
-	amazon, linode = menus.main_menu()
+	amazon, linode, self_hosted = menus.main_menu()
 	if amazon == True and linode == False:
 		conn = ec2funky.ec2connx(accesskey, secretkey)
 		iid, aid = menus.image_menu()
@@ -41,15 +42,20 @@ try:
 		# bulds instance information dictionary of the launched instance
 		iinfo_dic = ec2funky.instance_info(iid, conn)
 
+		# Launch SET Web Attacks
+		setweb.set_web_attacks(iinfo_dic)
+	
+		# Local Cleanup 
+		fabfunky.clean_local()
+
 	elif linode == True and amazon == False:
 		print "Support for linode is coming soon.  Please use AWS for now."
 		sys.exit(0)
 
-	# Launch SET Web Attacks
-	setweb.set_web_attacks(iinfo_dic)
-	
-	# Local Cleanup 
-	fabfunky.clean_local()
+	elif self_hosted == True:
+		ip = raw_input("Please enter the IP address of the Self hosted attack box: ")
+		self_dic = core.lib.selfy.self_info(ip)
+		print self_dic
 
 # Keyboard inturrupt exception
 except KeyboardInterrupt:
