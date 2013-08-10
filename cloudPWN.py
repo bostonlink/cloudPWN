26ea1a2211742a7b56854094c921fab4b5ed39b7
David Bressler (@bostonlink) <bostonlink@pentest-labs.org>
2013-08-10 09:45:20 -0400

    updated for self hosting options

--- cloudPWN.py
+++ cloudPWN.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-# CloudPWN source
+# cloudPWN source
 
 import sys
 import core.config
 import core.lib.ec2funky as ec2funky
 import core.lib.fabfunky as fabfunky
-import core.modules.setweb as setweb
 import core.menus as menus
 import core.lib.selfy
+from core.modules.setweb.charvest import charvest_launch
+from core.modules.setweb.java_applet_default import java_applet
+from core.modules.setweb.java_applet_pyinj import java_pyi
+from core.modules.cleanup import cleanupz
 from fabric.colors import red, yellow
 
 __author__ = 'David Bressler (@bostonlink), GuidePoint Security LLC'
@@ -30,22 +33,40 @@ instance_type = config["instance_type"]
 
 # Select what service to launch and get the instance id
 try:
+
 	amazon, linode, self_hosted = menus.main_menu()
+	
 	if amazon == True and linode == False:
+		
 		conn = ec2funky.ec2connx(accesskey, secretkey)
 		iid, aid = menus.image_menu()
 
-		# Launched new instance from user selection
+		# launches new instance and sets the image id of the instance
 		if iid == None:
 			iid = ec2funky.new_instance_launch(aid, conn, securitykey, instance_type, security_group)
 
 		# bulds instance information dictionary of the launched instance
 		iinfo_dic = ec2funky.instance_info(iid, conn)
 
-		# Launch SET Web Attacks
-		setweb.set_web_attacks(iinfo_dic)
+		# Launches SET Web Attacks
+		java_app_pyi, java_app, charvest = menus.autoset_menu()
 	
-		# Local Cleanup 
+		if java_app_pyi == True:
+			
+			java_pyi(iinfo_dic, config['instance_user'])
+		
+		elif java_app == True:
+
+			java_applet(iinfo_dic, config['instance_user'])
+
+		elif charvest == True:
+
+			charvest_launch(iinfo_dic, config['instance_user'])
+
+		# remote log pull and terminiation of instance
+		cleanupz(iinfo_dic, config['instance_user'])
+	
+		# Local temp file cleanup 
 		fabfunky.clean_local()
 
 	elif linode == True and amazon == False:
